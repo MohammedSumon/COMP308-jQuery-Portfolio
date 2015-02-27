bbe1e71e189b92915054314fdc8baa586ba497d8
MohammedSumon <mrmosumon@gmail.com>
2015-02-27 05:11:16 -0500

    inline comments added

--- jQuery Mobile Website/Scripts/mobile.js
+++ jQuery Mobile Website/Scripts/mobile.js
@@ -1,7 +1,14 @@
-﻿function init() {   
+﻿/*
+   jQuery Mobile Portfolio Website
+   Mohammed Sumon
+   Mohammed Sumon's Mobile Website
+   This is my javascript file for personal mobile website 
+*/
+function init() {
 
 }
 
+// Initialize the slider
 $(function () {
     $("#slider").excoloSlider();
 });
