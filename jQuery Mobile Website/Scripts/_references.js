c38565c40b8c5d13567eb2015c46b0f8669459bd
MohammedSumon <mrmosumon@gmail.com>
2015-02-27 20:54:09 -0500

    Final Commit

---
+++ jQuery Mobile Website/Scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="jquery-1.8.0.min.js" />
+/// <reference path="jquery-1.9.1.js" />
+/// <reference path="jquery.excoloSlider.js" />
+/// <reference path="jquery.mobile-1.4.5.js" />
+/// <reference path="mobile.js" />
+/// <reference path="swiper.min.js" />
